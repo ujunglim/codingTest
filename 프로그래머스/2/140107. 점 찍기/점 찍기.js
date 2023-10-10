1b1357dd7a5aa6a9b679dfc667293b50ae126450
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-10-10 14:07:24 +0900

    [level 2] Title: 점 찍기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,11 +1,9 @@
-// 간단히 생각
-// x를 순회하면서 최대y값 구하기
 function solution(k, d) {
     var answer = 0;
-    // k배씩 증가
-    for (let x = 0; x <= d; x=x+k) {
-        const maxY = Math.sqrt(d*d - x*x);
-        answer += Math.floor(maxY/k)+1;
+    
+    for (let x = 0; x <= d; x+=k) {
+        const maxY = Math.floor(Math.sqrt(d*d - x*x));
+        answer += Math.floor(maxY / k)+1; // +1
     }
     return answer;
 }
