2fd75c8717366139fc70ef46338eab7e5d03bc55
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-10-11 17:17:12 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.07 ms
+메모리: 33.4 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 16:53:42
+2023년 10월 3일 17:17:8
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,26 +1,22 @@
 function solution(progresses, speeds) {
     var answer = [];
-    const days = [];
-    for (let i = 0; i < progresses.length; i++) {
-        days.push(Math.ceil((100-progresses[i])/speeds[i])); // ceil
-    }
-    let queue = [];
-    let prevMax = 0;
-    for (const day of days) {
+    const needArr = progresses.map((p, i) => Math.ceil((100-p) / speeds[i]));
+    const queue = [];
+    
+    for (const need of needArr) {
         if (!queue.length) {
-            queue.push(day);
-            prevMax = day;
+            queue.push(need);
+            answer.push(1)
             continue;
         }
-        // 바로 전과 비교하는게 아니라 전까지 최대를 비교
-        if (prevMax >= day) {
-            queue.push(day);
+        // 전보다 시간이 더 많이 걸릴 경우
+        if (queue[queue.length-1] < need) {
+            queue.push(need);
+            answer.push(1);
         } else {
-            prevMax = day;
-            answer.push(queue.length);
-            queue = [day]; // 
+            answer[answer.length-1] += 1;
         }
     }
-    return [...answer, queue.length]; // queue에 남은 것 넣어주기
+    
+    return answer;
 }
-
