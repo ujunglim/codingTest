023ac6e16463dc9eeaee4b0766ae907e418035e8
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-23 20:38:16 +0900

    [level 3] Title: 부대복귀, Time: 91.21 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,47 +1,32 @@
 function solution(n, roads, sources, destination) {
-    var answer = [];
-    const dist = Array.from({length: n+1}, () => Infinity);
-    dist[destination] = 0;
+    // 그래프생성
+    const graph = {};
+    roads.forEach(([to, from]) => {
+        if (!(to in graph)) {
+            graph[to] = [];
+        }
+        if (!(from in graph)) {
+            graph[from] = [];
+        }
+        graph[to].push(from);
+        graph[from].push(to);
+    })
+    // weightArr, queue 초기화
+    const weightArr = Array.from({length: n+1}, () => Infinity);
+    weightArr[destination] = 0;
     const queue = [];
     queue.push(destination);
     
-    // roads를 object로 변환하기 (시작하는 포인트와 상관없이 순회하기 위해
-    const objRoads = {}; // {from : new Set(to1, to2..)}
-    roads.forEach(road => {
-        const [from, to] = road;
-        if (!objRoads[from]) {
-            objRoads[from] = new Set([to]);
-        } else {
-            objRoads[from].add(to);
-        }
-        // 반대의 경우
-        if (!objRoads[to]) {
-            objRoads[to] = new Set([from]);
-        } else {
-            objRoads[to].add(from);
-        }
-    })
-    
     while(queue.length) {
-        const curr = queue.shift(); // 현재노드
-        const neighbors = objRoads[curr]; // 현재노드와 연결된 노드들
+        const curr = queue.shift();
         
-        for (const neighbor of Array.from(neighbors)) {
-            const acc = dist[curr] + 1;
-            if (acc < dist[neighbor]) {
-                dist[neighbor] = acc;
-                queue.push(neighbor);
+        graph[curr].forEach((nextNode) => {
+            const acc = weightArr[curr] + 1;
+            if (weightArr[nextNode] > acc) {
+                weightArr[nextNode] = acc;
+                queue.push(nextNode);
             }
-        }
+        })
     }
-    return sources.map(source => {
-        if(dist[source] === Infinity) {
-            return -1;
-        }
-        return dist[source]
-    });
+    return sources.map(source => weightArr[source] === Infinity ? -1 : weightArr[source])
 }
-
-
-
-// visitised???
