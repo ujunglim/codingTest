d75e850a59fa6b9ddac5b5c25cb69ebe0092089e
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-23 20:15:57 +0900

    [level 2] Title: 배달, Time: 0.82 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,29 +1,31 @@
 function solution(N, road, K) {
-    var answer = 0;
-    const dist = Array.from({length: N+1}, () => Infinity);
-    // const visited = Array.from({length: N+1}, () => false);
-    // 마을 1번은 이미 방문했고 1에서 1까지 거리는 0이다
-    dist[1] = 0;
-    // visited[1] = true;
-    const queue = [{to: 1, dist: 0}];
-    
-    // graph정리
-    const graph = Array.from({length: N+1}, () => [])
-    for (const [from, to, dist] of road) {
-        graph[from].push({to, dist});
-        graph[to].push({to: from, dist}); // 양방향
-    }
+    // 그래프생성
+    const graph = {};
+    road.forEach(r => {
+        const [from, to, weight] = r;
+        if (!(from in graph)) {
+            graph[from] = [];
+        }
+        if (!(to in graph)) {
+            graph[to] = [];
+        }
+        graph[from].push({nextNode: to, nextWeight: weight});
+        graph[to].push({nextNode: from, nextWeight: weight});
+    })
+    const weightArr = Array.from({length: N+1}, () => Infinity);
+    const queue = [];
+    queue.push({currNode: 1, currWeight: 0});
+    weightArr[1] = 0;
     
     while(queue.length) {
-        const {to} = queue.shift(); 
-        graph[to].forEach(next => {
-            const acc = dist[to] + next.dist;
-            if (dist[next.to] > acc) {
-                dist[next.to] = acc; // 최단거리 갱신
-                queue.push(next)// 방문하기
+        const {currNode, currWeight} = queue.shift();
+        graph[currNode].forEach(({nextNode, nextWeight}) => {
+            const acc = currWeight + nextWeight;
+            if (acc < weightArr[nextNode]) {
+                weightArr[nextNode] = Math.min(weightArr[nextNode], acc);
+                queue.push({currNode: nextNode, currWeight: acc})
             }
         })
     }
-    return dist.filter(d => d <= K).length;
+    return weightArr.filter(weight => weight <= K).length;
 }
-
