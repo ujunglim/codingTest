e2f796859f2dbaa323c12f54ea3c923f36347b68
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-11-24 09:37:33 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 83.79 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,34 +1,62 @@
-function solution(queue1, queue2) {
-    let answer = 0;
-    const maxCount = queue1.length*3; // queue1, queue2 길이가 같으니까 최대이동횟수는 queue1에 있던걸 모두 queue2에 옮기고, queue2에서 마지막 하나빼고 다 queue1으로 옮기는 경우다
-    let sum1 = queue1.reduce((acc, curr) => acc + curr, 0);
-    let sum2 = queue2.reduce((acc, curr) => acc + curr, 0);
-    let index1 = 0;
-    let index2 = 0;
-    if ((sum1+sum2) / 2 === 1) return -1; // 둘의 합이 홀수 이면 -1
-    
-    // [탈출조건] 두 큐의 합이 같거나 최대이동횟수를 넘을때
-    while(sum1 !== sum2 && answer < maxCount) {
-        // queue2의 합이 더 큰 경우, queue2에서 shift한걸 queue1에 push
-        if (sum1 < sum2) {
-            const element = queue2[index2];
-            queue1.push(element); // queue2의 첫번째 값을 queue1에 넣기
-            sum1 += element; 
-            sum2 -= element;
-            index2++; // shift대신
-        } 
-        // queue1의 합이 더 큰 경우, queue1에서 shift한걸 queue2에 push
-        else {
-            const element = queue1[index1];
-            queue2.push(element);
-            sum2 += element;
-            sum1 -= element;
-            index1++;
-        }
-        answer++;
-    }
-    return sum1 === sum2 ? answer : -1;
+class Node {
+  constructor(value = 0, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
-// 시간초과, 탈출조건
-// shift는 O(n)으로 시간초과 => push만 하고 shift할 위치는 인덱스로 기억
-// 횟수, dfs?
+
+class Queue {
+  constructor(list = []) {
+    this.head = null;
+    this.p = this.head;
+    list.forEach((element) => this.enqueue(element));
+    this.size = 0;
+  }
+
+  enqueue(element) {
+    const newNode = new Node(element);
+    if (!this.head) {
+      this.head = newNode;
+      this.p = newNode;
+    } else {
+      this.p.next = newNode;
+      this.p = this.p.next;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    const dequeueValue = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return dequeueValue;
+  }
+}
+
+function solution(queue1, queue2) {
+  let answer = -1;
+  const n = queue1.length;
+  let sum1 = queue1.reduce((acc, curr) => acc + curr, 0);
+  let sum2 = queue2.reduce((acc, curr) => acc + curr, 0);
+  const q1 = new Queue(queue1);
+  const q2 = new Queue(queue2);
+
+  while (answer < 3 * n) {
+    answer++;
+    if (sum1 === sum2) {
+      return answer;
+    }
+    if (sum1 < sum2) {
+      const dequeueValue = q2.dequeue();
+      q1.enqueue(dequeueValue);
+      sum1 += dequeueValue;
+      sum2 -= dequeueValue;
+    } else {
+      const dequeueValue = q1.dequeue();
+      q2.enqueue(dequeueValue);
+      sum1 -= dequeueValue;
+      sum2 += dequeueValue;
+    }
+  }
+  return sum1 === sum2 ? answer : -1;
+}
