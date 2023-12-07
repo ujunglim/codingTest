13e42098760539a4a9af821e7a085e280a08bcd4
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-07 18:54:49 +0900

    [level 2] Title: 롤케이크 자르기, Time: 108.30 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,33 +1,40 @@
-// 1,000,000니까 순회를 최대한 줄여야한다.
+// 매번 자르면서 동생과 형 둘의 토핑갯수를 세는건 불필요하다
+// 처음부터 형한테 모든 토핑을 주고 그 후 토핑을 순회하며 동생이 하나씩 가져가면서 형과 동생의 토핑갯수를 비교한다.
+// 그냥 첨부터 순회하면서 하면 비교하면 안 되나?
 function solution(topping) {
     var answer = 0;
-    const left = {};
-    const right = {};
-    let leftCount = new Set(topping).size;
-    let rightCount = 0;
+    const old = {}; // 토핑 종류별 갯수
+    const young = {};
+    let oldCount = 0; // 토핑 종류 갯수
+    let youngCount = 0;
     
-    // left에 모든 토핑을 준다
-    topping.forEach(top => {
-        if (!left[top]) {
-            left[top] = 1;
+    for (const t of topping) {
+        if (!old[t]) {
+            old[t] = 1;
+            oldCount++;
         } else {
-            left[top] += 1;
+            old[t]++;
         }
-    })
+    }
     
-    // topping을 순회하며 하나씩 right한테 준다, 이때 이 둘의 토핑갯수를 비교한다.
-    topping.forEach(top => {
-        if (!right[top]) {
-            right[top] = 1;
-            rightCount++;
+    for (const t of topping) {
+        if (!young[t]) {
+            young[t] = 1;
+            youngCount++;
         } else {
-            right[top] += 1;
+            young[t]++;
         }
-        if (left[top] === 1) {
-            leftCount--;
+        
+        // 토핑이 한개 남아있으면 형의 토핑 갯수는 감소한다.
+        if (old[t] === 1) {
+            oldCount--;
         }
-        left[top] -= 1;
-        if (leftCount === rightCount) answer++;
-    })
+        old[t]--; // 형의 토핑 감소
+        
+        // 형과 동생의 토핑갯수 비교
+        if (oldCount === youngCount) {
+            answer++;
+        }
+    }
     return answer;
 }
