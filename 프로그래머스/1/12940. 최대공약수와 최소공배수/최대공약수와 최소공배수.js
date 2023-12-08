ba4c635a9a1f2818f656316a3bf6277f9b1976ef
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-08 14:47:16 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -4,16 +4,12 @@ function solution(n, m) {
 }
 
 function getGCD(a, b) {
-    const max = Math.max(a, b);
-    const min = Math.min(a, b);
-    while(b > 0) {
-        const smaller = b;
-        b = a%b;
-        a = smaller;
-    }
-    return a;
-}
-
-function getLCM(a, b) {
-    
+  let max = Math.max(a, b);
+  let min = Math.min(a, b);
+  while(min > 0) {
+      const smaller = min;
+      min = max%min;
+      max = smaller;
+  }
+  return max;
 }
