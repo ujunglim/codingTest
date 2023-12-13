afdbd0fb4ef1bd3bf37a9857dee0d9626493489f
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-13 14:37:40 +0900

    [Silver V] Title: 셀프 넘버, Time: 248 ms, Memory: 11944 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -0,0 +1,28 @@
+function solution() {
+  const max = 10000;
+  const isSelfNum = Array.from({ length: max + 1 }, () => true);
+
+  for (let i = 1; i < max; ++i) {
+    let sum = i + getSum(i);
+    if (sum <= max) {
+      isSelfNum[sum] = false;
+    }
+  }
+
+  for (let i = 1; i < max; ++i) {
+    if (isSelfNum[i] === true) {
+      console.log(i);
+    }
+  }
+}
+
+function getSum(n) {
+  if (n < 10) return n;
+  return (n % 10) + getSum(Math.floor(n / 10));
+}
+
+function getSum(n) {
+  if (n < 10) return n;
+  return (n % 10) + getSum(Math.floor(n / 10));
+}
+solution();
