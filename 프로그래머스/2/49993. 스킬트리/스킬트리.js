22f4d7f7acf70c665f04dea19c734ad82f085461
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-16 16:43:56 +0900

    [level 2] Title: 스킬트리, Time: 3.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,25 +1,12 @@
-// 테케3번째 BDA에서 B를 배우기전에 C를 먼제 배워야한다.
-// skill순서대로 출현하는지 확인
 function solution(skill, skill_trees) {
-    var answer = 0;
-    for (const tree of skill_trees) {
-        let isRight = true;
-        const required = [...skill];
-        for (let i = 0; i < tree.length; i++) {
-            const curr = tree[i];
-            if (!required.includes(curr)) continue; // 없는 스킬이면 패스
-            if (required.shift() === curr) continue; // 순서가 맞는 스킬이면 패스
-            // 순서가 안 맞음
-            else {
-                isRight = false;
-                break;
-            }
-            
+    function check(tree) {
+        const test = [...skill];
+        for (const s of tree) {
+            if (!skill.includes(s)) continue; // 스킬이외의 문자는 패스
+            if (s === test.shift()) continue; // 스킬순서가 맞으면 패스
+            return false; // 스킬순서가 맞지 않으면 false
         }
-        if (isRight) {
-            console.log(tree)
-            answer++;
-        }
+        return true;
     }
-    return answer;
+    return skill_trees.filter(check).length;
 }
