846ec26b4144252688e87aa496fa91d738fd880e
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-16 16:43:50 +0900

    [level 2] Title: 스킬트리, Time: 3.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,47 +1,23 @@
+// 테케3번째 BDA에서 B를 배우기전에 C를 먼제 배워야한다.
+// skill순서대로 출현하는지 확인
 function solution(skill, skill_trees) {
     var answer = 0;
-    let queue = [];
-    const obj = {};
-    const firstSkill = skill[0];
-    Array.from(skill).forEach((s, i) => obj[s] = i+1);
-    
-    // 트리
     for (const tree of skill_trees) {
-        let isTree = true;
-        // 트리 안의 스킬
-        for (const s of tree) {
-            console.log(tree, s, queue, isTree)
-            if (!obj[s]) continue; // 스킬이외 문자는 스킵
-            // 첫스킬
-            if (!queue.length) {
-                // 첫 스킬이 틀리면 다음 트리로
-                if (s !== firstSkill) {
-                    isTree = false;
-                    queue = [];
-                    break;
-                }
-                // 첫 스킬이 맞음 queue에 넣고 다음 스킬 검사
-                queue.push(s);
-            } else {
-                const prev = queue[queue.length-1];
-                // 미수행 선행스킬이 있을시
-                if (obj[prev]+1 !== obj[s]) {
-                    isTree = false;
-                    queue = [];
-                    break;
-                }
-                // 선행스킬을 수행했을때
-                queue.push(s);
-            }
-            // 이미 스킬 다 했을때
-            if (queue.length === skill.length) {
+        let isRight = true;
+        const required = [...skill];
+        for (let i = 0; i < tree.length; i++) {
+            const curr = tree[i];
+            if (!required.includes(curr)) continue; // 없는 스킬이면 패스
+            if (required.shift() === curr) continue; // 순서가 맞는 스킬이면 패스
+            // 순서가 안 맞음
+            else {
+                isRight = false;
                 break;
             }
+            
         }
-        // 스킬트리가 맞다
-        if (isTree) {
-            console.log('------')
-            queue = [];
+        if (isRight) {
+            console.log(tree)
             answer++;
         }
     }
