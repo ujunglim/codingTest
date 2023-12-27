257915f860114ed2f5226eb087efdcd0a90c7771
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-27 09:35:05 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 53.62 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,100 +1,49 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.front = null;
-    this.rear = null;
-    this.size = 0;
-  }
-
-  enqueue(val) {
-    const node = new Node(val);
-    if (!this.front) {
-      this.front = node;
-      this.rear = node;
-    } else {
-      this.rear.next = node;
-      this.rear = node;
-    }
-    return ++this.size;
-  }
-
-  dequeue() {
-    if (!this.front) return null;
-    const node = this.front;
-
-    if (this.front === this.rear) {
-      this.rear.next = null;
-    }
-
-    this.front = this.front.next;
-    this.size--;
-    return node.val;
-  }
-}
-
 function solution(land) {
-  const COL = land[0].length;
-  const ROW = land.length;
-
-  const dr = [0, 0, 1, -1];
-  const dc = [1, -1, 0, 0];
-
-  const q = new Queue();
-
-  //시추관을 뚫었을 때 각 컬럼에서 접근 가능한 석유 덩어리의 크기를 저장하기 위한 배열
-  const oilArr = new Array(COL).fill(0);
-
-  //모든 배열을 돌면서 석유 덩어리 순회
-  for (let r = 0; r < ROW; r++) {
-    for (let c = 0; c < COL; c++) {
-      //석유가 없는 땅일 경우 다음 칸으로 이동
-      if (!land[r][c]) continue;
-
-      //방문한 컬럼을 저장하기 위한 변수
-      const visitedCol = new Set();
-      //석유 덩어리의 크기를 저장하기 위한 변수
-      let oilSize = 0;
-
-      q.enqueue({ currentR: r, currentC: c });
-      //방문한 칸은 0으로 수정하여 방문처리
-      land[r][c] = 0;
-      oilSize++;
-      visitedCol.add(c);
-
-      //큐가 빌 때까지 반복
-      while (q.size) {
-        const { currentR, currentC } = q.dequeue();
-
-        for (let i = 0; i < 4; i++) {
-          const nextR = currentR + dr[i];
-          const nextC = currentC + dc[i];
-          if (
-            nextR >= 0 &&
-            nextR < ROW &&
-            nextC >= 0 &&
-            nextC < COL &&
-            land[nextR][nextC]
-          ) {
-            q.enqueue({ currentR: nextR, currentC: nextC });
-            land[nextR][nextC] = 0;
-            oilSize++;
-            visitedCol.add(nextC);
-          }
+    const dR = [-1, 0, 1, 0];
+    const dC = [0, 1, 0, -1];
+    const rLen = land.length;
+    const cLen = land[0].length;
+    const oilByCol = {}; //{열: 뽑을 수 있는 석유량}
+    
+    for (let c = 0; c < cLen; ++c) {
+        const queue = [];
+        
+        // 한 열에서 row를 순회하며 석유 찾기
+        for (let r = 0; r < rLen; ++r) {
+            // 석유를 찾음. 연결된 총 석유량 구해서 oilByCol에 더하기
+            if (land[r][c] === 1) {
+                land[r][c] = 0; // 이미 뽑은 석유로 변경
+                let sum = 1; // 석유량 초기화
+                let connectedCols = new Set(); // 현재위치와 연결된 열의 set
+                connectedCols.add(c);
+                queue.push({r, c});
+                
+                // 현재위치와 연결된 열과 총 석유량 구하기
+                while(queue.length) {
+                    const {r, c} = queue.shift();
+                    for (let i = 0; i < 4; ++i) {
+                        const nextR = r + dR[i];
+                        const nextC = c + dC[i];
+                        
+                        if (nextR >= 0 && nextR < rLen 
+                            && nextC >= 0 && nextC < cLen 
+                            && land[nextR][nextC] === 1
+                           ) {
+                            // 주변에 석유가 있고 새로운 열이면 연결된 열set에 추가
+                            if (!connectedCols.has(nextC)) connectedCols.add(nextC);
+                            sum++;
+                            land[nextR][nextC] = 0;
+                            queue.push({r: nextR, c: nextC}); // 주변열의 다음 주변을 확인한다.
+                        }
+                    }
+                }
+                // oilByCol의 연결된 열에 현재 석유그룹의 총 석유량 더하기
+                for (const c of connectedCols) {
+                    oilByCol[c] = (oilByCol[c] || 0) + sum;
+                }  
+            }
         }
-      }
-
-      //덩어리의 크기를 구한 다음, 방문한 컬럼별로 석유 덩어리 크기 oilArr에 더해줌
-      Array.from(visitedCol).forEach((c) => {
-        oilArr[c] += oilSize;
-      });
     }
-  }
-
-  return Math.max(...oilArr);
+    const values = Array.from(Object.values(oilByCol));
+    return Math.max(...values);
 }
