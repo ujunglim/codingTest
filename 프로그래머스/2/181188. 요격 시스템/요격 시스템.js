290ae2862482e50f8aa22863179060eab3ff6fde
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-28 12:26:58 +0900

    [level 2] Title: 요격 시스템, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,24 +1,20 @@
-// (s, e)만 가능하다. s,e는 포함하지 않는다
-// s, e사이 실수 가능
-// 최소
-// 가장 많이 겹치는 부분을 찾아야함
-// sort하고 겹치는 부분을 찾는다 a[1] > b[0]
-// 현재까지 겹치는구간의 마지막과 그 다음구간의 시작만 비교하면 된다 
-// prevEnd > currStart이면 겹친구간을 찾고 prevEnd를 갱신한다.
 function solution(targets) {
-    var answer = 0;
-    targets.sort((a, b) => a[1]-b[1]);
-    let prevEnd = targets[0][1];
-    
-    for(let i = 1; i < targets.length; i++) {
-        const [currStart, currEnd] = targets[i];
-        // 겹침
-        if (prevEnd > currStart) {
-            prevEnd = Math.min(prevEnd, currEnd);
-        } else {
-            answer++;
-            prevEnd = currEnd;
-        }
+  //타겟의 끝점을 기준으로 오름차순 정렬
+  targets.sort((a, b) => a[1] - b[1]);
+
+  //요격 횟수
+  let cnt = 0;
+  //요격 범위 끝점
+  let missile = -Infinity;
+
+  targets.forEach(([start, end]) => {
+    //타겟의 시작점이 요격범위보다 크거나 같으면(개구간)
+    //미사일 추가하고 범위 조정
+    if (start >= missile) {
+      cnt++;
+      missile = end;
     }
-    return answer+1; // 마지막
+  });
+
+  return cnt;
 }
