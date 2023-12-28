3c440ba666b259a2d1891a84a80edb9b2b0d9a97
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-28 12:13:53 +0900

    [level 2] Title: 요격 시스템, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -3,22 +3,21 @@
 // 최소
 // 가장 많이 겹치는 부분을 찾아야함
 // sort하고 겹치는 부분을 찾는다 a[1] > b[0]
+// 현재까지 겹치는구간의 마지막과 그 다음구간의 시작만 비교하면 된다 
+// prevEnd > currStart이면 겹친구간을 찾고 prevEnd를 갱신한다.
 function solution(targets) {
     var answer = 0;
-    targets.sort((a, b) => {
-        if (a[0] === b[0]) return a[1] - b[1];
-        return a[0]-b[0];
-    })
-    let prevRange = targets[0];
+    targets.sort((a, b) => a[1]-b[1]);
+    let prevEnd = targets[0][1];
     
     for(let i = 1; i < targets.length; i++) {
-        const currRange = targets[i];
+        const [currStart, currEnd] = targets[i];
         // 겹침
-        if (prevRange[1] > currRange[0]) {
-            prevRange = [currRange[0], prevRange[1] > currRange[1] ? currRange[1] : prevRange[1]];
+        if (prevEnd > currStart) {
+            prevEnd = Math.min(prevEnd, currEnd);
         } else {
             answer++;
-            prevRange = currRange;
+            prevEnd = currEnd;
         }
     }
     return answer+1; // 마지막
