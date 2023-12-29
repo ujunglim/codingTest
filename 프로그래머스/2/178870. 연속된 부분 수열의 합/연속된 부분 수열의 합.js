7fe97a8be53c78251f91f149d37410897ad15c3f
ujunglim <37919853+ujunglim@users.noreply.github.com>
2023-12-29 10:14:50 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,40 +1,28 @@
+// 짧은, 앞 인덱스
+// nlogn
 function solution(sequence, k) {
     var answer = [];
+    const len = sequence.length;
     let i = 0;
     let j = 0;
-    const len = sequence.length;
     let sum = sequence[0];
     
     while(i < len && j < len) {
-        if (sum < k) {
-            if (i === j) {
-                sum = sequence[i];
-            }
-            j++;
-            sum += sequence[j];
-        } else if (sum > k) {
-            sum -= sequence[i];
-            i++;
-        }
-         // 찾음
         if (sum === k) {
-            answer.push({
-                start: i,
-                end: j,
-                diff: j-i
-            });
-            j++;
-            sum += sequence[j];
+            answer.push({i, j, dist: j-i});
+            sum -= sequence[i++];
+        } else if (sum < k) {
+            sum += sequence[++j];
+        } else if (sum > k) {
+            sum -= sequence[i++];
         }
     }
-    answer.sort((a, b) => a.diff - b.diff);
-    const shortestDiff = answer[0].diff;
-    const shortests = answer.filter(a => a.diff === shortestDiff);
-    if (shortests.length > 1) {
-        shortests.sort((a, b) => a.start - b.start);
-        return [shortests[0].start, shortests[0].end];
-    } else {
-        return [answer[0].start, answer[0].end]; 
-    }
-    return answer;
+    if (answer.length === 1) return [answer[0].i, answer[0].j];
+    answer.sort((a, b) => {
+        if (a.dist === b.dist) {
+            return a - b;
+        } 
+        return a.dist - b.dist;
+    })
+    return [answer[0].i, answer[0].j];
 }
