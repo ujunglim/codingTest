fd817eae05d952ed32f064e9b139ba3bcf2b1847
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-08 15:09:47 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,26 +1,23 @@
-// x+n,  x*2, x*3
-// x를 y로 변환하기 위해 필요한 최소 연산 횟수
-
-// 세 연산의 모든 조합
-// 숫자가 y초과하면 더이상 연산할 필요없다
-
-// 시간초과 => dp
-
+// 최소 bfs
 function solution(x, y, n) {
-    let answer = Infinity;
-    const dp = Array.from({length: y+1}, () => Infinity); //dp[i] i값에 도달하기 위한 최소 연산 횟수
-    dp[x] = 0;
+    var answer = -1;
+    const queue = [{num: y, count: 0}];
     
-    for (let i = x; i <= y; ++i) {
-        if (dp[i+n]) {
-            dp[i+n] = Math.min(dp[i]+1, dp[i+n]);
+    while(queue.length) {
+        const {num, count} = queue.shift();
+        if (num === x) {
+            answer = count;
+            break;
+        } 
+        if (num%3 === 0) {
+            queue.push({num: num/3, count: count+1});
         }
-        if (dp[i*2]) {
-            dp[i*2] = Math.min(dp[i]+1, dp[i*2]);
+        if (num%2 === 0) {
+            queue.push({num: num/2, count: count+1});
         }
-        if (dp[i*3]) {
-            dp[i*3] = Math.min(dp[i]+1, dp[i*3]);
+        if (num-n >= x) {
+            queue.push({num: num-n, count: count+1});
         }
     }
-    return dp[y] === Infinity ? -1 : dp[y];
+    return answer;
 }
