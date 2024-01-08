6417c85154e6fdb814ddc155d2f9d5c43188b7dd
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-08 14:22:19 +0900

    [level 2] Title: 숫자 변환하기, Time: 103.17 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,15 +1,26 @@
-// 처음에 백트래킹, dfs풀이였다. 시간초과, 런타임에러
-// 최소경우의 수 dp arr[i] => i값에 도달하기 위한 최소값
+// x+n,  x*2, x*3
+// x를 y로 변환하기 위해 필요한 최소 연산 횟수
+
+// 세 연산의 모든 조합
+// 숫자가 y초과하면 더이상 연산할 필요없다
+
+// 시간초과 => dp
 
 function solution(x, y, n) {
-    if (x === y) return 0; // 이미x,y의 값이 같을때
-    const dp = Array.from({length: y+1}, () => Infinity);
-    dp[x] = 0; 
+    let answer = Infinity;
+    const dp = Array.from({length: y+1}, () => Infinity); //dp[i] i값에 도달하기 위한 최소 연산 횟수
+    dp[x] = 0;
     
     for (let i = x; i <= y; ++i) {
-        dp[i+n] = Math.min(dp[i+n], dp[i]+1);
-        dp[i*2] = Math.min(dp[i*2], dp[i]+1);
-        dp[i*3] = Math.min(dp[i*3], dp[i]+1);
+        if (dp[i+n]) {
+            dp[i+n] = Math.min(dp[i]+1, dp[i+n]);
+        }
+        if (dp[i*2]) {
+            dp[i*2] = Math.min(dp[i]+1, dp[i*2]);
+        }
+        if (dp[i*3]) {
+            dp[i*3] = Math.min(dp[i]+1, dp[i*3]);
+        }
     }
     return dp[y] === Infinity ? -1 : dp[y];
 }
