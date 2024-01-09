269fdd52bb4cc6d854cee082c30819875089cf20
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-09 15:03:00 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,16 +1,38 @@
+// 시소짝꿍: 몸무게*거리가 같으면 
+// 몇쌍 짝꿍존재?
+
+// 2,3,4 모든 조합
+// 시소의 오른쪽, 왼쪽 중 어디에 앉는지는 구별하지 않음
+// 몸무게가 같으면 어느 거리에 앉든 1쌍으로 쳐줌
+// 몸무게갯수 {}로 관리
+// 그럼 100, 100, 100처럼 100이 세명이면 3쌍? 아니면 1쌍
+
+// 4,3,2 || 2,3,4
+// 가능한 비율 1:1, 2:3, 1:2, 3:4
+
 function solution(weights) {
     var answer = 0;
-    const hash = {};
-    weights.sort((a, b) => a - b); // 
-    weights.forEach(w => hash[w] = (hash[w] || 0) + 1);
-     
-    for (const w of weights) {
-        if (hash[w] > 1) answer += hash[w]-1; // 자신을 포함해 2개이상 일시 짝꿍수를 더한다
-        if (hash[2*w]) answer += hash[2*w];
-        if (hash[w*4/3]) answer += hash[w*4/3];
-        if (hash[w*3/2]) answer += hash[w*3/2];
-        
-        hash[w] -= 1; // 짝꿍끼리의 순서는 상관없다 따라서 다음차례에서 현재 w를 비교대상에서 제외시킨다
+    const possibles = [1, 3/2, 2, 4/3];
+    const obj = {};
+    weights.forEach(weight => {
+        obj[weight] = (obj[weight] || 0) +1;
+    })
+    weights.sort((a, b) => a-b);
+    
+    for (const leftW of weights) {
+        for (const p of possibles) {
+            const rightW = leftW*p;
+            
+            // 같은 몸무게이면 
+            if (leftW === rightW) {
+                if (obj[rightW] > 1) {
+                    answer += obj[leftW]-1;
+                }
+            } else if (obj[rightW]) {
+                answer += obj[rightW];
+            }
+        }
+        obj[leftW]--;
     }
     return answer;
 }
