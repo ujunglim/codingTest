85d4c0da5aeef2188d88b2394bf2b5e2a4ac45d7
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-12 18:24:08 +0900

    [level Lv.1] Title: 최소직사각형, Time: 6.10 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/Lv.1/86491. 최소직사각형/최소직사각형.js
@@ -1,22 +1,19 @@
 // 유한한 경우 => 완탐
 // 다 담을 수 있는 가장 작은 지갑 크기
 // 가장 큰 가로, 세로 구함
-// 가로를 큰 기준으로 돌림
+// 큰 기준으로 미리 돌림
 function solution(sizes) {
     var answer = 0;
-    const w = [];
-    const h = [];
-    for (const [width, height] of sizes) {
-        if (width >= height) {
-            w.push(width);
-            h.push(height);
+    const big = [];
+    const small = [];
+    for (const [w, h] of sizes) {
+        if (w >= h) {
+            big.push(w);
+            small.push(h);
         } else {
-            w.push(height);
-            h.push(width);
+            big.push(h);
+            small.push(w);
         }
     }
-    const maxW = Math.max(...w);
-    const maxH = Math.max(...h);
-    console.log(maxW, maxH)
-    return maxW*maxH;
+    return Math.max(...big) * Math.max(...small);
 }
