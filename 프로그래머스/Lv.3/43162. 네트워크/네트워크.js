64425b2d0cbf7168b6dbe4b5e0d6617f476cd4bc
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-13 11:54:10 +0900

    [level Lv.3] Title: 네트워크, Time: 4.60 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43162. 네트워크/네트워크.js
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.js
@@ -1,23 +1,42 @@
-// 현재와 연결된 곳은 먼저 방문
+// 하나의 이어진 네트워크 중 가장 작은 수를 부모라고하면, 부모의 갯수 = 네트워크 갯수
 function solution(n, computers) {
-    var answer = 0;
-    const visited = Array.from({length: n}, () => false);
+    const parents = Array.from({length: n}, (_, i) => i);
     
-    for(let curr = 0; curr < n; curr++) {
-        if (!visited[curr]) {
-            visited[curr] = true;
-            answer++;
-            dfs(curr); // 현재와 연결된 곳 모두 방문
+    function find(n) {
+        if (parents[n] === n) return n;
+        parents[n] = find(parents[n]); // !
+        return parents[n];
+    }
+    
+    function union(a, b) {
+        const aParent = find(a);
+        const bParent = find(b);
+        
+        if (aParent < bParent) {
+            parents.forEach((parent, i) => {
+                if (parent === bParent) {
+                    parents[i] = aParent;
+                }
+            })
+        } else {
+            parents.forEach((parent, i) => {
+                if (parent === aParent) {
+                    parents[i] = bParent;
+                }
+            })
         }
     }
     
-    function dfs(curr) {
-        for (let i = 0; i < n; i++) {
-            if (i !== curr && computers[curr][i] === 1 && !visited[i]) {
-                visited[i] = true;
-                dfs(i);
+    for (let r = 0; r < n; ++r) {
+        // 대각선 기준으로 대칭이므로 c = r+1부터 확인하여 반복을 줄인다
+        for (let c = r+1; c < n; ++c) {
+            // r기준 현재와 이어졌고 부모가 다른 c를 찾으면 부모를 통일한다
+            if (computers[r][c] === 1 && find(r) !== find(c)) {
+                union(r, c);
             }
         }
+        
     }
-    return answer;
+    console.log(parents)
+    return new Set(parents).size
 }
