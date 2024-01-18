7c3c2472766424eb1a21f03effb3f0945a1ef955
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-18 11:24:57 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,20 +1,32 @@
-// 철수의 공약수 && 영희는 못 나눔
-// 영희의 공약수 && 철수는 못 나눔
-// 철수를 전부 나눌 수 있는 공약수의 최대값은 철수의 최솟값보다 작거나 같아야한다 (나눠야하니까)
-
+// 조건중 하나를 만족하는 가장 큰 양의 정수리턴
+// 조건1: 철수의 숫자를 모두 나눌 수 있고, 영희의 수를 하나도 나눌 수 없는 수
+// 조건2: 조건1반대
 function solution(arrayA, arrayB) {
-    var answer = 0;
     arrayA.sort((a, b) => a-b);
     arrayB.sort((a, b) => a-b);
+    
     return Math.max(findA(arrayA, arrayB), findA(arrayB, arrayA));
 }
 
 function findA(canDivide, cantDivide) {
-    const maxGCD = canDivide[0];
-    for (let i = maxGCD; i > 1; --i) {
-        if (canDivide.every(d => d % i === 0) && cantDivide.every(d => d % i !== 0)) {
-            return i;
+    // 나눌 수 있는 그룹의 최대공약수구하기
+    let gcd = canDivide[0];
+    for (let i = 0; i < canDivide.length; i++) {
+        gcd = getGCD(canDivide[i], gcd);
+    }
+    if (gcd === 1) return 0;
+    // 공약수가 1이거나 cantDivide를 나눌 수 있으면 O리턴
+    for (const e of cantDivide) {
+        if (e % gcd === 0) {
+            console.log(e % gcd)
+            return 0;
         }
     }
-    return 0;
+    return gcd;
+}
+
+function getGCD(a, b) {
+    const remainder = a % b;
+    if (remainder === 0) return b;
+    return getGCD(b, remainder);
 }
