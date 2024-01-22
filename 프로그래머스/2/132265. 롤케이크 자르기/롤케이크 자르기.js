e082e46ebbdbf2257c7ea43ae17ad07389b494a5
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-22 21:00:48 +0900

    [level 2] Title: 롤케이크 자르기, Time: 93.89 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,42 +1,32 @@
-// 토핑은 일렬로 올려져있다
-// 동일한 토핑종류의 갯수를 갖게 나누는 방법 수
-
-// 둘로 나눔
-// topping 길이 1,000,000 => O( nlogn )
-// slice의 시간복잡도는 O(n) => 매번 잘라서 형과 동생의 토핑갯수를 구할 수 없다
-
-// 반으로 나누는게 아니라 토핑의 갯수를 비교하는거임
-// delete obj
-
+// o(nlogn)
+// 동일한 토핑 갯수
 function solution(topping) {
     var answer = 0;
     const old = {};
     const young = {};
-    topping.forEach(t => {
-        old[t] = (old[t] || 0) + 1;
-    })
-    let oldCount = Object.keys(old).length;
+    let oldCount;
     let youngCount = 0;
-    
+    topping.forEach(t => {
+        old[t] = (old[t] || 0) +1;
+    })
+    oldCount = Object.keys(old).length;
     for (const curr of topping) {
-        // 동생은 없는 토핑
+        // 동생이 없던거
         if (!young[curr]) {
-            youngCount++;
             young[curr] = 1;
+            youngCount++;
         } else {
-            young[curr]++; // 동생이 있는 토핑
+            young[curr]++;
         }
-        // 형이 하나 밖에 없는 토핑이면
+        
+        // 형이 해당 토핑을 1개만 갖고 있는 경우
         if (old[curr] === 1) {
             delete old[curr];
             oldCount--;
         } else {
             old[curr]--;
         }
-        // 동생과 형의 토핑갯수가 같을시
-        if (oldCount === youngCount) {
-            answer++;
-        }
+        if (oldCount === youngCount) answer++;
     }
     return answer;
 }
