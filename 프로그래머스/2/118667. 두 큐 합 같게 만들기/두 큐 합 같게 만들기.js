dfab6fec53c992a9028e1cafbdb7fc33eb89fffb
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-24 14:08:25 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 16.59 ms, Memory: 47.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,62 +1,32 @@
-class Node {
-  constructor(value = 0, next = null) {
-    this.value = value;
-    this.next = next;
-  }
-}
-
-class Queue {
-  constructor(list = []) {
-    this.head = null;
-    this.p = this.head;
-    list.forEach((element) => this.enqueue(element));
-    this.size = 0;
-  }
-
-  enqueue(element) {
-    const newNode = new Node(element);
-    if (!this.head) {
-      this.head = newNode;
-      this.p = newNode;
-    } else {
-      this.p.next = newNode;
-      this.p = this.p.next;
-    }
-    this.size++;
-  }
-
-  dequeue() {
-    const dequeueValue = this.head.value;
-    this.head = this.head.next;
-    this.size--;
-    return dequeueValue;
-  }
-}
-
+// 두개의 큐
+// 큐의 원소의 합이 같게 하는 최소pop, insert 횟수를 리턴
+// 모든 경우구함
 function solution(queue1, queue2) {
-  let answer = -1;
-  const n = queue1.length;
-  let sum1 = queue1.reduce((acc, curr) => acc + curr, 0);
-  let sum2 = queue2.reduce((acc, curr) => acc + curr, 0);
-  const q1 = new Queue(queue1);
-  const q2 = new Queue(queue2);
-
-  while (answer < 3 * n) {
-    answer++;
-    if (sum1 === sum2) {
-      return answer;
+    let count = 0;
+    const maxCount = queue1.length*3;
+    let sum1 = queue1.reduce((acc, curr) => acc + curr, 0);
+    let sum2 = queue2.reduce((acc, curr) => acc + curr, 0);
+    let index1 = 0;
+    let index2 = 0;
+    
+    if ((sum1+sum2)%2 === 1) return -1; // 두 수의 합이 홀수이면 -1리턴
+    
+    while(sum1 !== sum2 && count < maxCount) {
+        // queue2의 합이 더 클때 shift해서 queue1에 추가한다.
+        if (sum1 < sum2) {
+            const move = queue2[index2];
+            queue1.push(move);
+            sum1 += move;
+            sum2 -= move;
+            index2++;
+        } else {
+            const move = queue1[index1];
+            queue2.push(move);
+            sum2 += move;
+            sum1 -= move;
+            index1++;
+        }
+        count++;
     }
-    if (sum1 < sum2) {
-      const dequeueValue = q2.dequeue();
-      q1.enqueue(dequeueValue);
-      sum1 += dequeueValue;
-      sum2 -= dequeueValue;
-    } else {
-      const dequeueValue = q1.dequeue();
-      q2.enqueue(dequeueValue);
-      sum1 -= dequeueValue;
-      sum2 += dequeueValue;
-    }
-  }
-  return sum1 === sum2 ? answer : -1;
+    return sum1 === sum2 ? count : -1;
 }
