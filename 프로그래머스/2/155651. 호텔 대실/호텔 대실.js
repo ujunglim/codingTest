d5005d7e6230166badd937059a37b3dad113709e
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-25 09:59:12 +0900

    [level 2] Title: 호텔 대실, Time: 4.16 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -2,34 +2,39 @@
 // 필요한 최소객실수는?
 // 분으로 변환
 // 시작시각 오름차순
+// 들어갈 수 있는 시각을 저장하는 배열 (오름차순)
+
 function solution(book_time) {
-  var answer = 0;
-  const bookArr = book_time
-    .map((book) => {
-      const converted = book.map((b) => {
-        const [h, m] = b.split(":");
-        return Number(h) * 60 + Number(m);
-      });
-      return { start: converted[0], end: converted[1] };
-    })
-    .sort((a, b) => a.start - b.start);
-
-  answer++; // 첫번째방
-  const endTime = [bookArr[0].end+10];
-
-  for (let i = 1; i < bookArr.length; ++i) {
-    const { start, end } = bookArr[i];
-    // 대실시작시각에 퇴실한 방이 없는 경우
-    if (start < endTime[0]) {
-      answer++; // 방추가
-      endTime.push(end+10);
+    var answer = 0;
+    const bookArr = book_time.map(book => {
+        const convertToNum = book.map(b => {
+            const [h, m] = b.split(':');
+            return Number(h)*60+Number(m);
+        })
+        return {start: convertToNum[0], end: convertToNum[1]};
+    }).sort((a, b) => a.start - b.start);
+    
+    const canEnterTimes = [bookArr[0].end+10];
+    answer++; // 첫번째방
+    
+    for (let i = 1; i < bookArr.length; ++i) {
+        const {start, end} = bookArr[i];
+        // 들어갈 수 있는 방 찾기
+        let createRoom = true;
+        for (let j = 0; j < canEnterTimes.length;j++) {
+            // 방 찾음
+            if (canEnterTimes[j] <= start) {
+                canEnterTimes.splice(j, 1);
+                canEnterTimes.push(end+10);
+                createRoom = false;
+                break;
+            }
+        }
+        
+        if (createRoom) {
+            answer++;
+            canEnterTimes.push(end+10);
+        }
     }
-    // 대실시작시각에 퇴실한 방이 있는 경우
-    else {
-      endTime.shift(); // 이미 퇴실한 방 제거
-      endTime.push(end+10);
-    }
-    endTime.sort((a, b) => a - b); // 가장빠른퇴실시각 갱신
-  }
-  return answer;
+    return answer;
 }
