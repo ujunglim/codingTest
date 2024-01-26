a74300623773601201b9995ade3162d86dd0c120
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-26 22:04:10 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,11 +1,19 @@
+// k칸 점프, 현재까지 온거리 *2로 순간이동
+// k칸 점프를 하면 k만큼 사용량, 순간이동은 없음
+// 점프이동 최소
+// 10억 이하 자연수 => logN
+// 첫 점프하고
+// 2분할되면 나누고 안 될때마다 +점프
 function solution(n)
 {
-    let result = 0;
+    var ans = 0;
     while(n > 0) {
-        if (n%2 === 1) {
-            result++;
+        if (n % 2 === 0) {
+            n /= 2;
+        } else {
+            ans++;
+            n--;
         }
-        n = Math.floor(n/2);
     }
-    return result;
+    return ans;
 }
