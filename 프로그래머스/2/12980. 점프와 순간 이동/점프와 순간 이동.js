a0ee85de2cd4eb8c6d62a400b0b6563b50f4b62f
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-01-26 22:05:46 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -8,12 +8,10 @@ function solution(n)
 {
     var ans = 0;
     while(n > 0) {
-        if (n % 2 === 0) {
-            n /= 2;
-        } else {
+        if (n % 2 === 1) {
             ans++;
-            n--;
-        }
+        } 
+        n = Math.floor(n/2);
     }
     return ans;
 }
