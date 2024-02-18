b433fb80452942813e7585c9f0a56f9f3ac3995f
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-02-18 18:09:13 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 13.64 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,32 +1,34 @@
-// 두개의 큐
-// 큐의 원소의 합이 같게 하는 최소pop, insert 횟수를 리턴
-// 모든 경우구함
+// 두 큐의 합이 같게 하기 위해 뺐다 넣다해야하는 최소횟수
+// 안 되면 -1
+// 둘의 합에서 빼주고 더해주고를 반복
 function solution(queue1, queue2) {
-    let count = 0;
-    const maxCount = queue1.length*3;
-    let sum1 = queue1.reduce((acc, curr) => acc + curr, 0);
-    let sum2 = queue2.reduce((acc, curr) => acc + curr, 0);
-    let index1 = 0;
-    let index2 = 0;
+    var answer = 0;
+    let sum1 = getSum(queue1);
+    let sum2 = getSum(queue2);
+    let p1 = 0;
+    let p2 = 0;
+    const max = queue1.length * 3;
     
-    if ((sum1+sum2)%2 === 1) return -1; // 두 수의 합이 홀수이면 -1리턴
-    
-    while(sum1 !== sum2 && count < maxCount) {
-        // queue2의 합이 더 클때 shift해서 queue1에 추가한다.
-        if (sum1 < sum2) {
-            const move = queue2[index2];
-            queue1.push(move);
-            sum1 += move;
-            sum2 -= move;
-            index2++;
+    for (let i = 0; i < max; i++) {
+        if (sum1 === sum2) return answer;
+        if (sum1 > sum2) {
+            const movingNum = queue1[p1];
+            sum1 -= movingNum;
+            sum2 += movingNum;
+            queue2.push(movingNum)
+            p1++;
         } else {
-            const move = queue1[index1];
-            queue2.push(move);
-            sum2 += move;
-            sum1 -= move;
-            index1++;
+            const movingNum = queue2[p2];
+            sum1 += movingNum;
+            queue1.push(movingNum);
+            sum2 -= movingNum;
+            p2++;
         }
-        count++;
+        answer++;
     }
-    return sum1 === sum2 ? count : -1;
+    return -1;
+}
+
+function getSum(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
 }
