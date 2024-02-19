677662e2d66694df90dbdfa918183a5ece476955
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-02-19 13:12:21 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 7.85 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
@@ -1,102 +1,37 @@
-class Heapq {
-    constructor() {
-        this.heap = [null];
-    }
-
-    heappush(data) {
-        const [c_time, c_alp, c_cop] = data;
-        let current = this.heap.length;
-        while (current > 1) {
-            const parent = Math.floor(current / 2);
-            if (this.heap[parent].cur_time > c_time) {
-                this.heap[current] = this.heap[parent];
-                current = parent;
-            }
-            else {
-                break;
-            }
-        }
-
-        this.heap[current] = { 'cur_time': c_time, 'cur_alp': c_alp, 'cur_cop': c_cop };
-    }
-
-    heappop() {
-        let min = this.heap[1];
-
-        if (this.heap.length > 2) {
-            this.heap[1] = this.heap[this.heap.length - 1];
-            this.heap.splice(this.heap.length - 1);
-
-            let current = 1;
-            let leftChildIndex = current * 2;
-            let rightChildIndex = current * 2 + 1;
-
-            while (this.heap[leftChildIndex]) {
-                let childIndexToCompare = leftChildIndex;
-                if (this.heap[rightChildIndex] && this.heap[rightChildIndex].cur_time < this.heap[childIndexToCompare].cur_time) {
-                    childIndexToCompare = rightChildIndex;
-                }
-                if (this.heap[current].cur_time > this.heap[childIndexToCompare].cur_time) {
-                    [this.heap[current], this.heap[childIndexToCompare]] = [this.heap[childIndexToCompare], this.heap[current],];
-                    current = childIndexToCompare;
-                }
-                else {
-                    break;
-                }
-                leftChildIndex = current * 2;
-                rightChildIndex = current * 2 + 1;
-            }
-        }
-        else if (this.heap.length === 2) {
-            this.heap.splice(1, 1);
-        }
-        else {
-            return null;
-        }
-        return min;
-    }
-}
-
+// 알고력, 코딩력 1을 높이기 위해 1의 시간이 필요2
+// 누적 dp
 function solution(alp, cop, problems) {
-    let answer = 0;
-    let max_alp = 0;
-    let max_cop = 0;
-    problems.forEach(problem => {
-        max_alp = max_alp > problem[0] ? max_alp : problem[0];
-        max_cop = max_cop > problem[1] ? max_cop : problem[1];
+    let maxAlp = alp;
+    let maxCop = cop;
+    problems.forEach(([alp, cop]) => {
+        maxAlp = Math.max(maxAlp, alp);
+        maxCop = Math.max(maxCop, cop);
     });
-
-    if (alp >= max_alp && cop >= max_cop) {
-        return 0;
-    }
-
-    const pq = new Heapq();
-    const visited = Array(151).fill().map(() => Array(151).fill(false));
-
-    problems.push([0, 0, 1, 0, 1]);
-    problems.push([0, 0, 0, 1, 1]);
-
-    pq.heappush([0, alp, cop]);
-
-    while (pq.heap.length - 1 != 0) {
-        const { cur_time, cur_alp, cur_cop } = pq.heappop();
-        if (visited[cur_alp][cur_cop]) {
-            continue;
-        }
-        visited[cur_alp][cur_cop] = true;
-
-        if (max_alp <= cur_alp && max_cop <= cur_cop) {
-            return cur_time;
-        }
-
-        problems.forEach(([alp_req, cop_req, alp_rwd, cop_rwd, cost]) => {
-            if (cur_alp >= alp_req && cur_cop >= cop_req) {
-                next_alp = cur_alp + alp_rwd < max_alp ? cur_alp + alp_rwd : max_alp;
-                next_cop = cur_cop + cop_rwd < max_cop ? cur_cop + cop_rwd : max_cop;
-                pq.heappush([cur_time + cost, next_alp, next_cop]);
+    
+    const dp = Array.from({length: maxAlp+1}, () => Array.from({length: maxCop+1}, () => Infinity));
+    dp[alp][cop] = 0;
+    
+    for (let currAlp = alp; currAlp <= maxAlp; ++currAlp) {
+        for (let currCop = cop; currCop <= maxCop; ++currCop) {
+            // 알고력1을 높였을때 최대필요 알고력을 넘지 않으면 알고력1을 높인 시간을 갱신
+            if (currAlp+1 <= maxAlp) {
+                dp[currAlp+1][currCop] = Math.min(dp[currAlp+1][currCop] , dp[currAlp][currCop]+1);
             }
-        });
+            // 코딩력1을 높였을때 최대필요 코딩력을 넘지 않으면 코딩력1을 높인 시간을 갱신
+            if (currCop+1 <= maxCop) {
+                dp[currAlp][currCop+1] = Math.min(dp[currAlp][currCop+1], dp[currAlp][currCop]+1);
+            }
+            // 풀 수 있는 문제가 있음 풀고 시간 갱신
+            problems.forEach(([alp_req, cop_req, alp_rwd, cop_rwd, cost]) => {
+                // 풀 수 있는 문제다
+                if (currAlp >= alp_req && currCop >= cop_req) {
+                    // 문제를 풀어 점수를 얻어서
+                    const next_alp = Math.min(currAlp + alp_rwd, maxAlp);
+                    const next_cop = Math.min(currCop + cop_rwd, maxCop);
+                    dp[next_alp][next_cop] = Math.min(dp[currAlp][currCop] + cost, dp[next_alp][next_cop]);
+                }
+            })
+        }
     }
-    answer = 0;
-    return answer;
+    return dp[maxAlp][maxCop];
 }
