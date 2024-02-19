710262d212c68e408f56993099d045cc1dbaf5c4
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-02-19 11:17:42 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,29 +1,29 @@
+// n개 질문, 7개 선택지
+// 각 지표는 더 높은 점수, 같으면 사전순 빠른 순
 function solution(survey, choices) {
     var answer = '';
-    const types = ["RT", "CF", "JM", "AN"];
+    const type = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']];
     const scores = [3,2,1,0,1,2,3];
-    const obj = {};
+    const obj = {}; // 얻은 점수 정리
     
-    // 얻은 점수 정리
-    for (let i = 0; i < survey.length; ++i) {
-        const [sur1, sur2] = survey[i].split('');
-        const currChoice = choices[i];
-        const score = scores[currChoice-1]
-        if (currChoice < 4) {
-            obj[sur1] = (obj[sur1] || 0) + score;
-        } else if (currChoice > 4) {
-            obj[sur2] = (obj[sur2] || 0) + score;
+    for (let i = 0; i < survey.length; i++) {
+        const [first, second] = survey[i].split('');
+        const choice = choices[i];
+        if (choice < 4) {
+            obj[first] = (obj[first] || 0) + scores[choice-1];
+        } else if (4 < choice) {
+            obj[second] = (obj[second] || 0) + scores[choice-1];
         }
     }
-    
-    for (const type of types) {
-        const [t1, t2] = type.split('');
-        if (!obj[t2] || obj[t1] > obj[t2]) {
-            answer += t1;
-        } else if (!obj[t1] || obj[t1] < obj[t2]) {
-            answer += t2;
-        } else {
-            answer += [t1, t2].sort()[0];
+    for(const [first, second] of type) {
+        if (!obj[second] || obj[first] > obj[second]) {
+            answer += first;
+        } else if (!obj[first] || obj[first] < obj[second]) {
+            answer += second;
+        } 
+        // 사전순
+        else {
+            answer += [first, second].sort()[0];
         }
     }
     return answer;
