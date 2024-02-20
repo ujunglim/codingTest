afed42fe80917e56f15043082b0973660133bea5
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-02-20 15:43:35 +0900

    [level 3] Title: 등산코스 정하기, Time: 236.58 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.js
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.js
@@ -1,77 +1,147 @@
-// 작은intensity 순으로 정렬하는 minHeap
-class MinHeap {
-  constructor() {
-    this.heap = [null];
-  }
+// // 1~n, 출입구, 쉼터, 산봉우리, 양방향
+// // intensity: 휴식없이 이동하는 최장시간
+// // 출입구중 한 곳에서 출발, 산봉우리 한곳만 방문 후 다시 원래 출입구로 돌아옴
+// // intensity가 최소가 되는 [산봉우리번호, intensity 최소값] 리턴, intensity최소가 여러개이면 낮은 산봉우리
 
-  push(data) {
-    const { intensity, node } = data; ////////// {}
-    let currIndex = this.heap.length;
+// // 올라갔던 길을 다시 그대로 내려오면 된다
+// // 한 출입구에서 한 산봉우리까지 갈 수 있는 경우들 intensity 정리
+// // 최소 intensity 찾기
+// function solution(n, paths, gates, summits) {
+//     var answer = [];
+//     summits.sort((a, b) => a-b); // 낮은 산봉우리부터 
+//     const isSummit = new Set(summits); // 산봉우리인지 확인 O(1)
+    
+//     // 양방향 그래프 정리 {'1': [ { to: 2, intensity: 3 } ]}
+//     const graph = {};
+//     paths.forEach(([from, to, intensity]) => {
+//         // 새로운 노드면 초기화
+//         if (!graph[from]) graph[from] = [];
+//         if (!graph[to]) graph[to] = [];
+//         // 노드 당 연결된 path
+//         graph[from].push({nextNode: to, nextIntensity: intensity});
+//         graph[to].push({nextNode: from, nextIntensity: intensity});
+//     })
+    
+//     const minQ = new MinHeap();
+//     const intensityArr = Array.from({length: n+1}, () => Infinity); // i번 노드까지 가는데 필요한 최대 intensity
+    
+//     // 출입구부터 큐에 넣고 초기화한다
+//     for (const gate of gates) {
+//         minQ.push({currIntensity: 0, currNode: gate});
+//         intensityArr[gate] = 0; // 출입구의 가중치는 0
+//     }
+    
+//     while(minQ.size) {
+//         const {currNode, currIntensity} = minQ.pop(); // 현재노드, 현재노드 까지 필요한 최대가중치
+        
+//         // 현재노드가 정상이거나 현재 가중치 > 기존 가중치이면 현재노드에서 탐색을 멈춘다. /////////////
+//         if (isSummit(currNode) || currIntensity > intensityArr[currNode]) {
+//             continue;
+//         }
+        
+//         // 현재노드와 연결된 다음 노드들을 순회한다
+//         for (const {nextNode, nextIntensity} of graph[currNode]) {
+//             // 현재노드에서 다음노드로 가는 가중치와 현재까지 최대가중치 중 큰 값은 새로운 가중치이다.
+//             const newIntensity = Math.max(nextIntensity, currIntensity);
+//             // 새로운 가중치 < 기존의 가중치이면 
+//             if (newIntensity < intensityArr[nextNode]) {
+//                 // 기존의 가중치를 새로운 가중치로 갱신하고
+//                 intensityArr[nextNode] = newIntensity;
+//                 // 다음노드와 새로운 가중치를 큐에 추가한다. ////////////
+//                 minQ.push({currIntensity: newIntensity, currNode: newNode}); 
+//             }
+//         }
+//     }
+    
+//     console.log(intensityArr);
+//     return answer;
+// }
 
-    // 부모노드가 존재할때 현재노드의 제 위치를 찾는다
-    while (currIndex > 1) {
-      const parentIndex = Math.floor(currIndex / 2);
-      // 부모 > 현재이면 swap
-      if (this.heap[parentIndex].intensity > intensity) {
-        [this.heap[currIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currIndex]]
-        currIndex = parentIndex;
-      } else {
-        break;
-      }
-    }
-    this.heap[currIndex] = { intensity, node };
-  }
+// class MinHeap {
+//   constructor() {
+//     this.heap = [null];
+//   }
 
-  pop() {
-    const min = this.heap[1];
+//   push(data) {
+//     const { intensity, node } = data; ////////// {}
+//     let currIndex = this.heap.length;
 
-    // 자식노드가 있을떄
-    if (this.heap.length > 2) {
-      // 마지막 노드를 최상위 노드로 올려준다
-      this.heap[1] = this.heap.pop();
-      let currIndex = 1;
-      let leftChildIndex = currIndex * 2;
-      let rightChildIndex = currIndex * 2 + 1;
+//     // 부모노드가 존재할때 현재노드의 제 위치를 찾는다
+//     while (currIndex > 1) {
+//       const parentIndex = Math.floor(currIndex / 2);
+//       // 부모 > 현재이면 swap
+//       if (this.heap[parentIndex].intensity > intensity) {
+//         [this.heap[currIndex], this.heap[parentIndex]] = [
+//           this.heap[parentIndex],
+//           this.heap[currIndex],
+//         ];
+//         currIndex = parentIndex;
+//       } else {
+//         break;
+//       }
+//     }
+//     this.heap[currIndex] = { intensity, node };
+//   }
 
-      // 자식이 있을 때
-      while (this.heap[leftChildIndex]) {
-        // 두 자식중 intensity최소자식 구하기
-        let smallerChildIndex = leftChildIndex;
-        if (
-          this.heap[rightChildIndex] &&
-          this.heap[leftChildIndex].intensity >
-            this.heap[rightChildIndex].intensity
-        ) {
-          smallerChildIndex = rightChildIndex;
-        }
-        // 부모 > 최소자식이면 둘을 swap
-        if (
-          this.heap[currIndex].intensity >
-          this.heap[smallerChildIndex].intensity
-        ) {
-          [this.heap[currIndex], this.heap[smallerChildIndex]] = [
-            this.heap[smallerChildIndex],
-            this.heap[currIndex],
-          ];
-          currIndex = smallerChildIndex;
-        } else {
-          break;
-        }
-        leftChildIndex = currIndex * 2;
-        rightChildIndex = currIndex * 2 + 1;
-      }
-      return min;
-    } else if (this.heap.length === 2) {
-      this.heap.pop();
-    } else {
-      return null;
-    }
-    return min;
-  }
-}
+//   pop() {
+//     const min = this.heap[1];
+
+//     // 자식노드가 있을떄
+//     if (this.heap.length > 2) {
+//       // 마지막 노드를 최상위 노드로 올려준다
+//       this.heap[1] = this.heap.pop();
+//       let currIndex = 1;
+//       let leftChildIndex = currIndex * 2;
+//       let rightChildIndex = currIndex * 2 + 1;
+
+//       // 자식이 있을 때
+//       while (this.heap[leftChildIndex]) {
+//         // 두 자식중 intensity최소자식 구하기
+//         let smallerChildIndex = leftChildIndex;
+//         if (
+//           this.heap[rightChildIndex] &&
+//           this.heap[leftChildIndex].intensity >
+//             this.heap[rightChildIndex].intensity
+//         ) {
+//           smallerChildIndex = rightChildIndex;
+//         }
+//         // 부모 > 최소자식이면 둘을 swap
+//         if (
+//           this.heap[currIndex].intensity >
+//           this.heap[smallerChildIndex].intensity
+//         ) {
+//           [this.heap[currIndex], this.heap[smallerChildIndex]] = [
+//             this.heap[smallerChildIndex],
+//             this.heap[currIndex],
+//           ];
+//           currIndex = smallerChildIndex;
+//         } else {
+//           break;
+//         }
+//         leftChildIndex = currIndex * 2;
+//         rightChildIndex = currIndex * 2 + 1;
+//       }
+//       return min;
+//     } else if (this.heap.length === 2) {
+//       this.heap.pop();
+//     } else {
+//       return null;
+//     }
+//     return min;
+//   }
+// }
+// https://school.programmers.co.kr/learn/courses/30/lessons/118669
+// 1~n, 출입구, 쉼터, 산봉우리, 양방향
+// intensity: 휴식없이 이동하는 최장시간
+// 출입구중 한 곳에서 출발, 산봉우리 한곳만 방문 후 다시 원래 출입구로 돌아옴
+// intensity가 최소가 되는 [산봉우리번호, intensity 최소값] 리턴, intensity최소가 여러개이면 낮은 산봉우리
+
+// 올라갔던 길을 다시 그대로 내려오면 된다
+// 한 출입구에서 한 산봉우리까지 갈 수 있는 경우들 intensity 정리
+// 최소 intensity 찾기
+
 function solution(n, paths, gates, summits) {
-  // 작은정상 우선
-  summits.sort((a, b) => a - b);
+  summits.sort((a, b) => a - b); // 작은정상 우선
   const summitSet = new Set(summits); // 정상인지 판단할때 사용
 
   // 그래프 정리
@@ -130,4 +200,79 @@ function solution(n, paths, gates, summits) {
     }
   });
   return answer;
+}
+
+// 작은intensity 순으로 정렬하는 minHeap
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(data) {
+    const { intensity, node } = data; ////////// {}
+    let currIndex = this.heap.length;
+
+    // 부모노드가 존재할때 현재노드의 제 위치를 찾는다
+    while (currIndex > 1) {
+      const parentIndex = Math.floor(currIndex / 2);
+      // 부모 > 현재이면 swap
+      if (this.heap[parentIndex].intensity > intensity) {
+        [this.heap[currIndex], this.heap[parentIndex]] = [
+          this.heap[parentIndex],
+          this.heap[currIndex],
+        ];
+        currIndex = parentIndex;
+      } else {
+        break;
+      }
+    }
+    this.heap[currIndex] = { intensity, node };
+  }
+
+  pop() {
+    const min = this.heap[1];
+
+    // 자식노드가 있을떄
+    if (this.heap.length > 2) {
+      // 마지막 노드를 최상위 노드로 올려준다
+      this.heap[1] = this.heap.pop();
+      let currIndex = 1;
+      let leftChildIndex = currIndex * 2;
+      let rightChildIndex = currIndex * 2 + 1;
+
+      // 자식이 있을 때
+      while (this.heap[leftChildIndex]) {
+        // 두 자식중 intensity최소자식 구하기
+        let smallerChildIndex = leftChildIndex;
+        if (
+          this.heap[rightChildIndex] &&
+          this.heap[leftChildIndex].intensity >
+            this.heap[rightChildIndex].intensity
+        ) {
+          smallerChildIndex = rightChildIndex;
+        }
+        // 부모 > 최소자식이면 둘을 swap
+        if (
+          this.heap[currIndex].intensity >
+          this.heap[smallerChildIndex].intensity
+        ) {
+          [this.heap[currIndex], this.heap[smallerChildIndex]] = [
+            this.heap[smallerChildIndex],
+            this.heap[currIndex],
+          ];
+          currIndex = smallerChildIndex;
+        } else {
+          break;
+        }
+        leftChildIndex = currIndex * 2;
+        rightChildIndex = currIndex * 2 + 1;
+      }
+      return min;
+    } else if (this.heap.length === 2) {
+      this.heap.pop();
+    } else {
+      return null;
+    }
+    return min;
+  }
 }
