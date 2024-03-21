7ddbe66e127ec1098dabc65666de123531676b0a
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-03-21 20:46:45 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -2,7 +2,7 @@
 function solution(n, k) {
   var answer = [];
   let arr = Array.from({ length: n }, (_, i) => i + 1);
-  const factorialArr = [1, 1];
+  const factorialArr = [1, 1]; // 각 자리마다 만들수 있는 조합의 수
 
   function getFactorialArr(n) {
     for (let i = 2; i <= n; ++i) {
@@ -16,8 +16,13 @@ function solution(n, k) {
       answer = acc.concat(arr);
       return;
     }
-    const selectedIndex = Math.floor((k - 1) / factorialArr[n - 1]);
+
+    let selectedIndex = Math.floor(k / factorialArr[n - 1]);
+    // const selectedIndex = Math.floor((k - 1) / factorialArr[n - 1]);
     k = k % factorialArr[n - 1];
+    if (k === 0) {
+      selectedIndex--;
+    }
 
     bt(n - 1, k, [...acc, ...arr.splice(selectedIndex, 1)]);
   }
