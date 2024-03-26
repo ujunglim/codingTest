a42bc942c6bb68b6841661b45101064c8550c0ab
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-03-26 10:40:12 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.js
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.js
@@ -1,35 +1,48 @@
+// 초당 최대 처리량: 1초당 처리하는 요청 최대 갯수
+// 응답완료시간 S와 처리시간 T가 공백
+// 완료시간 - 처리시간 + 0.001s
+// 초당최대처리량 리턴
+
+// 4.002-2+0.001 = 2.003시작 4.002끝
+// 7.000-2+0.001 = 5.001시작 7.000끝
+//=> 4.002에서 5.002사이 1초이하이므로 2개
+
+// 시간 ms로 변경, 끝나는 시각 정리
+// 시작시각 오름차순
+
 function solution(lines) {
-    var answer = -Infinity;
-    const timeline = [];
     let count = 0;
-    
+    let answer = 0;
+    const timeline = [];
     for (const line of lines) {
-        const [date, end, cost] = line.split(' ');
-        const [hour, minute, second] = end.split(':');
-        const timeToSecond = (Number(hour)*3600 + Number(minute)*60 + Number(second)) * 1000;
-        const costTime = Number(cost.substring(0, cost.length-1))*1000;
-        const startTime = timeToSecond - costTime + 1;
-        const endTime = timeToSecond + 999; //////////////// 왜 999를 더해???
+        const [date, endTime, duration] = line.split(' ');
+        const endTimeMs = convertToMS(endTime);
+        const durationMS = Number(duration.split('s')[0]) *1000;
+        const startTime = endTimeMs - durationMS + 1;
         
-        timeline.push(["start", startTime]);
-        timeline.push(["end", endTime]);
-    }
-    // sort
-    timeline.sort((a, b) => {
-        if (a[1] === b[1]) return -1; //a-b는 왜 안 돼????
-        return a[1] - b[1];
-    })
-    // console.log(timeline)
-    for (const time of timeline) {
-        if (time[0] === 'start') {
-            count++;
-        } else if (time [0] === 'end') {
-            count--;
+        timeline.push(['start', startTime]);
+        timeline.push(['end', endTimeMs+ 999]);
+        
+        timeline.sort((a, b) => {
+            if (a[1] === b[1]) { // 같으면 순서를 변경하지 않음
+                return -1;
+            }
+            return a[1] - b[1]; // 오름차순
+        })
+        
+        for (const time of timeline) {
+            if (time[0] === 'start') {
+                count++;
+            } else {
+                count--;
+            }
+            answer = Math.max(answer, count);
         }
-        answer = Math.max(answer, count);
     }
     return answer;
 }
 
-// 문자열
-// 밀리세컨즈로 통일
+function convertToMS(time) {
+    const [h, m, s] = time.split(':');
+    return (Number(h)*3600 + Number(m)*60 + Number(s))*1000;
+}
