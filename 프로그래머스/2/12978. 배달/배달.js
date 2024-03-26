f67840b500e57da94de416435a70126129b1e96f
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-03-26 13:16:51 +0900

    [level 2] Title: 배달, Time: 0.53 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,5 +1,5 @@
 function solution(N, road, K) {
-    // 그래프생성
+    // 양방향 그래프생성
     const graph = {};
     road.forEach(r => {
         const [from, to, weight] = r;
@@ -12,18 +12,21 @@ function solution(N, road, K) {
         graph[from].push({nextNode: to, nextWeight: weight});
         graph[to].push({nextNode: from, nextWeight: weight});
     })
-    const weightArr = Array.from({length: N+1}, () => Infinity);
+    const weightArr = Array.from({length: N+1}, () => Infinity); // 각 점마다 최단거리 초기화
     const queue = [];
-    queue.push({currNode: 1, currWeight: 0});
-    weightArr[1] = 0;
+    queue.push({currNode: 1, currWeight: 0}); // 시작점
+    weightArr[1] = 0; // 시작점 방문 표시
     
     while(queue.length) {
         const {currNode, currWeight} = queue.shift();
+        // 현재위치의 주변
         graph[currNode].forEach(({nextNode, nextWeight}) => {
             const acc = currWeight + nextWeight;
+            // 더 짧은 최단거리를 구하면 갱신
             if (acc < weightArr[nextNode]) {
-                weightArr[nextNode] = Math.min(weightArr[nextNode], acc);
-                queue.push({currNode: nextNode, currWeight: acc})
+                // weightArr[nextNode] = Math.min(weightArr[nextNode], acc);
+                weightArr[nextNode] = Math.min(acc);
+                queue.push({currNode: nextNode, currWeight: acc}); // 다음 노드로 이동
             }
         })
     }
