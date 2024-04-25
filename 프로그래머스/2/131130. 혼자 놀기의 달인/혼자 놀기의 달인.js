0b21fd41163a9c88cf40653488cc08b1242d4fd8
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-04-25 09:59:25 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
@@ -1,37 +1,30 @@
-// 1-100까지 카드, 2이상의 수를 정해 그 수보다 작거나 같은 숫자들을 준비하고 카드 수만큼의 상자를 준비
-// 임의로 하나 선택하여 그 안의 숫자 상자를 연다
-// 이미 열려있을 떄까지 반복, 
-// 1번 그룹을 제외하고 남는 상자가 없으면 게임 종료하고 이때 획득 점수는 0점
-// 그렇지 않으면 남은상자중 같은 방식으로 연다
-// 1번 상자수 & 2번 상자수 최고 점수를 리턴
+// 작거나 같은
+// 열어야 하는 상자가 이미 열려있을 때까지 반복
+// 1번 그룹을 제외하고 남는 상자가 없으면 게임 종료 0점
+// 또 임의를 골라 2번 상자그룹
+// 게임 점수 = 1번상자수 * 2번 상자수
 
-// 모든 상자 그룹을 찾고 최대 2개의 갯수를 곱함
-// 이미 열린건 0으로 표시
+// 완전탐색
 function solution(cards) {
     var answer = [];
     
-    for (let i = 0; i < cards.length; i++) {
-        const currNum = cards[i];
-        // 아직 안 쓴 카드인 경우 그룹을 찾는다
-        if (currNum !== 0) {
-            let group = [];
-            let index = i;
-            // 열려있는 상자를 찾을 떄까지
-            while(cards[index] !== 0) {
-                group.push(cards[index]);
-                const nextIndex = cards[index]-1;
-                cards[index] = 0; // 열린 표시
-                index = nextIndex;
-            }
-            if (group.length === cards.length) {
-                answer.push(0);
-                continue;
-            } else {
-                answer.push(group.length);
-                group = [];
-            }
+    function dfs(currIndex, count) {
+        // 이미 열은 상자
+        if (cards[currIndex] === 0) {
+            answer.push(count);
+            return;
+        }
+        const nextIndex = cards[currIndex]-1;
+        cards[currIndex] = 0;
+        dfs(nextIndex, count+1);
+    }
+    
+    for (let i = 0; i < cards.length; ++i) {
+        if (cards[i] !== 0) {
+            dfs(i, 0);
         }
     }
+    if (answer.length === 1) return 0;
     answer.sort((a, b) => b-a);
-    return answer.length >= 2 ? answer[0]*answer[1] : 0;
+    return answer[0] * answer[1];
 }
