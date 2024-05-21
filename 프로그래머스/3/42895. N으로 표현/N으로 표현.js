7d1c3f872f71951abd2fc70b187c22d86f9c483c
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-05-21 17:14:57 +0900

    [level 3] Title: N으로 표현, Time: 3.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,51 +1,51 @@
-// function solution(N, number) {
-//     const sets = Array.from({length: 9}, () => new Set());
-//     // 5, 55, 555 ...추가
-//     sets.forEach((set, i) => {
-//         set.add(Number(String(N).repeat(i)));
-//     })
-    
-//     console.log(sets)
-//     for (let i = 1; i <= 8; i++) {
-//         for (let j = 1; j <= i; j++) { //
-//             for (const first of sets[j]) {
-//                 for (const second of sets[i-j]) {
-//                     sets[i].add(first+second);
-//                     sets[i].add(first-second);
-//                     sets[i].add(first*second);
-//                     sets[i].add(Math.floor(first/second));
-//                 }
-//                 if (sets[i].has(number)) return i;
-//             }
-//         }
-//     }
-//     return -1;
-// }
-
 function solution(N, number) {
-    if (N === number) {
-        return 1;
-    }
-    const setArr = Array.from({length: 9}, () => new Set());
-    setArr.forEach((_, i) => setArr[i].add(Number(String(N).repeat(i))));
+    const sets = Array.from({length: 9}, () => new Set());
+    // 5, 55, 555 ...추가
+    sets.forEach((set, i) => {
+        set.add(Number(String(N).repeat(i)));
+    })
     
-    for (let totalCount = 2; totalCount <= 8; totalCount++) {
-        for (let firstCount = 1; firstCount < totalCount; firstCount++) {
-            for (const first of setArr[firstCount]) {
-                for (const second of setArr[totalCount-firstCount]) {
-                    setArr[totalCount].add(first+second);
-                    setArr[totalCount].add(first-second);
-                    setArr[totalCount].add(first*second);
-                    setArr[totalCount].add(Math.floor(first/second));
+    console.log(sets)
+    for (let i = 1; i <= 8; i++) {
+        for (let j = 1; j <= i; j++) { //
+            for (const first of sets[j]) {
+                for (const second of sets[i-j]) {
+                    sets[i].add(first+second);
+                    sets[i].add(first-second);
+                    sets[i].add(first*second);
+                    sets[i].add(Math.floor(first/second));
                 }
-                if (setArr[totalCount].has(number)) {
-                    return totalCount;
-                }
+                if (sets[i].has(number)) return i;
             }
-        } 
+        }
     }
     return -1;
 }
 
-// 5/0 = Infinity
-// Math.floor(5/0) = Infinity
+// function solution(N, number) {
+//     // if (N === number) {
+//     //     return 1;
+//     // }
+//     const setArr = Array.from({length: 9}, () => new Set());
+//     setArr.forEach((set, i) => set.add(Number(String(N).repeat(i))));
+    
+//     for (let totalCount = 1; totalCount <= 8; totalCount++) {
+//         for (let firstCount = 1; firstCount <= totalCount; firstCount++) {
+//             for (const first of setArr[firstCount]) {
+//                 for (const second of setArr[totalCount-firstCount]) {
+//                     setArr[totalCount].add(first+second);
+//                     setArr[totalCount].add(first-second);
+//                     setArr[totalCount].add(first*second);
+//                     setArr[totalCount].add(Math.floor(first/second));
+//                 }
+//                 if (setArr[totalCount].has(number)) {
+//                     return totalCount;
+//                 }
+//             }
+//         } 
+//     }
+//     return -1;
+// }
+
+// // 5/0 = Infinity
+// // Math.floor(5/0) = Infinity
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.08 ms
+메모리: 33.6 MB, 시간: 3.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 12:33:13
+2024년 05월 21일 17:14:51
 
 ### 문제 설명
 
