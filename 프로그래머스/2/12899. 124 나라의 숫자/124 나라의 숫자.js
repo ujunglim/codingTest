09718d1aebafa104112993e4fafb81f5edfdc024
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-05-23 15:19:35 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.20 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -4,25 +4,24 @@
 // 0이 아니면 몫의 124숫자 + 나머지
 
 function solution(n) {
-    var answer = [];
-    const dp = {};
-    
-    function convertNum(num) {
-        if (num <= 0) {
-            return;
-        }
-        const division = Math.floor(num / 3);
-        const remain = num % 3;
-        
-        if (remain === 0) {
-            answer.unshift(4);
-            convertNum(division-1);
-        } else {
-            answer.unshift(remain);
-            convertNum(division);
-        }
+  var answer = [];
+
+  function convertNum(num) {
+    if (num <= 0) {
+      return;
     }
-    
-    convertNum(n);
-    return answer.join('');
+    const division = Math.floor(num / 3);
+    const remain = num % 3;
+
+    if (remain === 0) {
+      answer.unshift(4);
+      convertNum(division - 1);
+    } else {
+      answer.unshift(remain);
+      convertNum(division);
+    }
+  }
+
+  convertNum(n);
+  return answer.join("");
 }
