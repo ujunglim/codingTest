5b0fb663fb4cade62aef8f7941b20a5721a9378a
ujunglim <37919853+ujunglim@users.noreply.github.com>
2024-07-14 09:46:59 +0900

    [level 2] Title: H-Index, Time: 3.68 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,14 +1,19 @@
 function solution(citations) {
-    let answer = 0;
-    citations.sort((a, b) => a - b);
-    const total = citations.length;
-    
-    for (let h = 0; h <= citations.length; ++h) {
-        const count = citations.filter(c => c >= h).length; ////
-        if (count >= h) {
-            answer = h;
-        }
-    }
-    return answer;
-}
+  let answer = 0;
+  citations.sort((a, b) => a - b);
+  const total = citations.length;
 
+  for (let h = 0; h <= citations.length; ++h) {
+    let i = 0;
+    // h미만까지 i옮기기
+    while (h > citations[i]) {
+      i++;
+    }
+    const count = total - i; // h이상 갯수
+    // h이상 갯수가 h이상이면 answer갱신
+    if (count >= h) {
+      answer = h;
+    }
+  }
+  return answer;
+}
